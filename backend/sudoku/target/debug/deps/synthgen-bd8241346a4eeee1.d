4db6731fb8ab0c5d2eee117d5127da45d087a9ab
Nikulin V. <viacheslav.nikulin@freedompay.kz>
2025-08-15 18:24:36 +0500

    remove /target

--- backend/sudoku/target/debug/deps/synthgen-bd8241346a4eeee1.d
+++
@@ -1,5 +0,0 @@
-/home/viacheslavnikulin/projects/puzzle-solver/backend/sudoku/target/debug/deps/synthgen-bd8241346a4eeee1.d: train/synthgen/src/main.rs
-
-/home/viacheslavnikulin/projects/puzzle-solver/backend/sudoku/target/debug/deps/synthgen-bd8241346a4eeee1: train/synthgen/src/main.rs
-
-train/synthgen/src/main.rs:
